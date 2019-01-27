a6069d438e33c797cf8667d1285ec2bdbe33bee3
zeynepduzyurt <zeynepduzyurt34@gmail.com>
2019-01-27 21:44:17 +0300

    Add files via upload

---
+++ TransactionTime.sql
@@ -0,0 +1,64 @@
+CREATE OR REPLACE PACKAGE hr.getTime AS 
+PROCEDURE  SelectOperation(inputNumber IN NUMBER,pin_value IN NUMBER);
+PROCEDURE  CalculateTime(pin_Operation IN NUMBER);
+END getTime;
+/
+
+CREATE OR REPLACE PACKAGE BODY hr.getTime AS
+PROCEDURE  SelectOperation(inputNumber IN NUMBER,pin_value IN NUMBER)
+AS 
+operation Number;
+ BEGIN 
+   IF(pin_value = 1) THEN 
+     DBMS_OUTPUT.PUT_LINE(inputNumber || 'adet tabloya ekleme yapýldý');
+     FOR loop_counter IN  1..inputNumber 
+    LOOP
+      INSERT INTO intern (name) VALUES('Zeynep'); 
+   END LOOP;
+    
+
+ ELSIF(pin_value = 2) THEN 
+  DBMS_OUTPUT.PUT_LINE(inputNumber ||' adet Tabloda silinme iþlemi yapýldý');
+     FOR loop_counter IN  1..inputNumber 
+    LOOP
+      DELETE FROM intern WHERE name = 'Zeynep';
+   END LOOP;
+   
+   
+    ELSIF(pin_value = 3) THEN 
+     DBMS_OUTPUT.PUT_LINE(inputNumber ||' adet satýr tabloda güncellendi');
+     FOR loop_counter IN  1..inputNumber 
+    LOOP
+     UPDATE intern SET name='Düzyurt';
+   END LOOP;
+   END IF; 
+   
+END SelectOperation;  
+PROCEDURE  CalculateTime (pin_Operation IN NUMBER)
+AS
+
+first_number Number; 
+final_number Number;
+BEGIN
+   
+   first_number := DBMS_UTILITY.GET_TIME;
+   IF(pin_Operation = 1) THEN
+    DBMS_OUTPUT.PUT_LINE('rollback yapýldý');
+    rollback;
+    ELSIF(pin_Operation =2) THEN
+    DBMS_OUTPUT.PUT_LINE('commit yapýldý');
+   commit;
+   END IF;
+   
+   final_number := DBMS_UTILITY.GET_TIME;
+   DBMS_OUTPUT.PUT_LINE(((final_number)-(first_number))/100 || 'sn');
+   
+END CalculateTime;
+END getTime;
+/
+
+
+Execute getTime.SelectOperation(100000,1);
+Execute getTime.CalculateTime(2);
+
+select * from intern;
